de9a2b59465ff43fc99d4ad8d2ec6da44c6e9a28
Swati Agrawal <swati.agrawal1312@gmail.com>
2025-12-29 17:14:10 +0530

    Time: 2 ms (43.59%) | Memory: 94.4 MB (45.68%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxprofit=0;
+        int buy_price=prices[0];
+        int curr_profit=0;
+        for(int i=1;i<prices.length;i++){
+            if(buy_price>prices[i]){
+                buy_price=prices[i];
+            }
+            else{
+                curr_profit=prices[i]-buy_price;
+                maxprofit=Math.max(maxprofit,curr_profit);
+
+            }
+        }
+        return maxprofit;
+        
+    }
+}
