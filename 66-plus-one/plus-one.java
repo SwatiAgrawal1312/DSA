f552c6d3c0060101694a13364067f85940e2a255
Swati Agrawal <swati.agrawal1312@gmail.com>
2026-01-01 18:04:40 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (37.47%) - LeetSync

---
+++ 66-plus-one/plus-one.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        
+      
+        int i=digits.length-1;
+        while(i>=0){
+            if(digits[i]<9){
+                digits[i]+=1;
+                return digits;
+            }
+            else if(digits[i]==9){
+                digits[i]=0;
+            }
+            i--;
+        }
+        int result[]=new int[digits.length+1];
+        result[0]=1;
+        return result;
+        
+
+
+       
+       
+       
+        
+    }
+}
