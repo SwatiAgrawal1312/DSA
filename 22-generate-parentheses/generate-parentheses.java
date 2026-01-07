ef6b562334c3d5cffd5bc178e420d85ebd97f05a
Swati Agrawal <swati.agrawal1312@gmail.com>
2026-01-07 20:16:12 +0530

    Time: 2 ms (68.59%) | Memory: 44.9 MB (23.68%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,36 +1,26 @@
 class Solution {
     List<String> res=new ArrayList<>();
     
-    public void helper(String curr,int n){
-        if(curr.length()==2*n){
-            if(isValid(curr)){
-                res.add(curr);
-            }
-           return;
-        }
-        helper(curr+"(",n);
-        helper(curr+")",n);
-       }
-    public boolean isValid(String curr){
-       
-        int count=0;
-        for(int i=0;i<curr.length();i++){
-            char ch=curr.charAt(i);
-            if(ch=='('){
-                count++;
-            }else {
-                count--;
-                if(count<0){
-                    return false;
-                }
-            }
-            
-        }
-        return count==0;
-    }
+    
+    
     public List<String> generateParenthesis(int n) {
        
-        helper("",n);
+        helper("",0,0,n);
         return res;
         }
+        public void helper(String curr,int open,int close ,int n){
+        if(curr.length()==2*n){
+           res.add(curr);
+           return;
+            }
+           
+        
+        if(open<n){
+          helper(curr+"(",open+1,close,n);
+        } 
+        if(close<open){
+           helper(curr+")",open,close+1,n);
+        } 
+       
+       }
     }
