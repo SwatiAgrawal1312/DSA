12c1b94e261fd971944b85473c48eccc65e62760
Swati Agrawal <swati.agrawal1312@gmail.com>
2026-01-20 13:36:43 +0530

    Time: 16 ms (14.91%) | Memory: 46.9 MB (30.70%) - LeetSync

--- 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.java
+++ 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.java
@@ -2,14 +2,13 @@ class Solution {
     public int[] minBitwiseArray(List<Integer> nums) {
         int[] result=new int[nums.size()];
         for(int i=0;i<nums.size();i++){
+            result[i]=-1;
             for(int j=0;j<1000;j++){
                 if((j|(j+1))==nums.get(i)){
                     result[i]=j;
                     break;
                 }
-                else{
-                    result[i]=-1;
-                }    
+                
             }
         }
         return result;
