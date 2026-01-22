020c3eb06b7f8d889ce34d75b1a6853e502126a9
Swati Agrawal <swati.agrawal1312@gmail.com>
2026-01-22 20:40:49 +0530

    Time: 2 ms (87.43%) | Memory: 43.9 MB (50.01%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        int last_idx[]=new int[26];
+        boolean seen[]=new boolean[26];
+        for(int i=0;i<s.length();i++){
+            last_idx[s.charAt(i)-'a']=i;
+        }
+        Stack<Character> st=new Stack();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(seen[ch-'a']){
+                continue;
+            }
+            
+            while(!st.isEmpty() && ch<st.peek() && last_idx[st.peek()-'a']>i){
+                seen[st.pop()-'a']=false;
+            }
+            st.push(ch);
+            seen[ch-'a']=true;
+        }
+        StringBuilder sb=new StringBuilder();
+        for(char c:st){
+            sb.append(c);
+
+        }
+        return sb.toString();
+        
+       
+
+        
+        
+    }
+}
