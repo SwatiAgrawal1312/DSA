718ce9e6ee96b147e201cc87e4cafbf753f6128c
Swati Agrawal <swati.agrawal1312@gmail.com>
2026-02-10 22:10:10 +0530

    Time: 395 ms (17.33%) | Memory: 47 MB (68.61%) - LeetSync

--- 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.java
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.java
@@ -5,15 +5,15 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             Set<Integer> even=new HashSet<>();
             Set<Integer> odd=new HashSet<>();
-            int j=i;
-            while(j<nums.length){
+           
+            for(int j=i;j<nums.length;j++){
             
             if(nums[j]%2==0) even.add(nums[j]);
             else odd.add(nums[j]);
             
             if(even.size()==odd.size()) result=Math.max(result,j-i+1);
             
-            j++;
+           
             }
             
 
