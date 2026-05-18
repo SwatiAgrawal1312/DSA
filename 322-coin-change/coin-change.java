2b604f0af20893dc473a789df5cbac3df8b17d93
Swati Agrawal <swati.agrawal1312@gmail.com>
2026-05-18 09:59:30 +0530

    Time: 11 ms (96.20%) | Memory: 46.4 MB (59.02%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,30 +1,16 @@
 class Solution {
-    Integer[][] dp;
-
     public int coinChange(int[] coins, int amount) {
-       dp=new Integer[coins.length][amount+1];
-        int result=solve(coins,amount,coins.length-1);
-        return result==Integer.MAX_VALUE?-1:result;
-        
-    }
-    public int solve(int[] coins,int amount,int i){
-        if(amount==0){
-            return 0;
-        }
-        if(i<0){
-            return Integer.MAX_VALUE;
-        }
-        if(dp[i][amount]!=null){
-            return dp[i][amount];
-        }
-        int take=Integer.MAX_VALUE;
-        if(coins[i]<=amount){
-            take=solve(coins,amount-coins[i],i);
-            if(take!=Integer.MAX_VALUE){
-                take=1+take;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+
+        dp[0] = 0;
+
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
             }
         }
-       int skip=solve(coins,amount,i-1);
-        return dp[i][amount]=Math.min(take,skip);
+
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 }
