81726902b573b4b58544dda6408e7f0c7f14218a
Swati Agrawal <swati.agrawal1312@gmail.com>
2026-05-20 12:31:38 +0530

    Time: 95 ms (5.14%) | Memory: 163.9 MB (5.12%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -1,36 +1,31 @@
 class Solution {
-    int n;
-    int dp[][];
+    int dp[][]=new int[1001][1001];
     public int longestPalindromeSubseq(String s) {
-        n=s.length()-1;
-        dp=new int[1001][1001];
-        for(int i=0;i<dp.length;i++){
-            for(int j=0;j<dp.length;j++){
-                if(i==j){
-                    dp[i][j]= 1;
-                }else{
-                    dp[i][j]=1000;
-                }
-            }
+       
+
+        String t=new StringBuilder(s).reverse().toString();
+        
+        for(int rows[]:dp){
+            Arrays.fill(rows,-1);
         }
-        return helper(s,0,n);
+       
+        return solver(s,t,0,0);
         
     }
-    public int helper(String s,int i,int j){
-        if(i==j){
-            return 1;
-        }
-        if(i>j){
+    public int solver(String s,String t,int i,int j){
+        int m=s.length();
+        int n=t.length();
+        if(i>=m || j>=n){
             return 0;
         }
-        if(dp[i][j]!=1000){
+        if(dp[i][j]!=-1){
             return dp[i][j];
         }
-        if(s.charAt(i)==s.charAt(j)){
-            return dp[i][j]=2+helper(s,i+1,j-1);
-        }else{
-            return dp[i][j]=Math.max(helper(s,i+1,j),helper(s,i,j-1));
+        if(s.charAt(i)==t.charAt(j)){
+            return dp[i][j]=1+solver(s,t,i+1,j+1);
         }
-        
+        int inc_i=solver(s,t,i+1,j);
+        int inc_j=solver(s,t,i,j+1);
+        return dp[i][j]=Math.max(inc_i,inc_j);
     }
 }
