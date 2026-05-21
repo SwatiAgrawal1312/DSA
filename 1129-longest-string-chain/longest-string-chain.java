f05ac0ef1258eecba1c68bf21aca243fd430e3fb
Swati Agrawal <swati.agrawal1312@gmail.com>
2026-05-21 15:22:41 +0530

    Time: 159 ms (5.87%) | Memory: 161.5 MB (5.10%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.java
+++ 1129-longest-string-chain/longest-string-chain.java
@@ -1,49 +1,44 @@
 class Solution {
-    int dp[][];
+    int dp[][]=new int[1001][1002];
     public int longestStrChain(String[] words) {
+       
         Arrays.sort(words,(a,b)->(a.length()-b.length()));
-        dp=new int[1000][1001];
         for(int rows[]:dp){
             Arrays.fill(rows,-1);
         }
-        
-        return helper(words,0,-1);
+        return solver(words,0,-1);
         
     }
-   
-    public int helper(String[] words,int curr_idx,int prev_idx ){
-        if(curr_idx==words.length){
-            return 0;
-        }
-        if(dp[curr_idx][prev_idx+1]!=-1){
-            return dp[curr_idx][prev_idx+1];
-        }
-        int skip=helper(words,curr_idx+1,prev_idx);
-        int take=0;
-        if(prev_idx==-1 || ispredecessor(words[prev_idx],words[curr_idx])){
-            take=1+helper(words,curr_idx+1,curr_idx);
-        }
-        return dp[curr_idx][prev_idx+1]=Math.max(skip,take);
-
-    }
-    public boolean ispredecessor(String st1,String st2){
-        int m=st1.length();
-        int n=st2.length();
-        if(n-m!=1){
+    public boolean check_predecessor(String first,String second){
+        if(second.length()!=first.length()+1){
             return false;
         }
         int i=0;
         int j=0;
-        while(i<m && j<n){
-            if(st1.charAt(i)==st2.charAt(j)){
+       while(i<first.length() && j<second.length()){
+            if(first.charAt(i)==second.charAt(j)){
                 i++;
                 j++;
+
             }else{
                 j++;
             }
         }
-        
-        return i==m;
-
+        return i==first.length();
     }
+    public int solver(String[] words,int i,int prev){
+        if(i==words.length){
+            return 0;
+        }
+        int take=0;
+        if(dp[i][prev+1]!=-1){
+            return dp[i][prev+1];
+        }
+        if(prev==-1|| check_predecessor(words[prev],words[i])==true){
+            take=1+solver(words,i+1,i);
+        }
+        int skip=solver(words,i+1,prev);
+        return dp[i][prev+1]=Math.max(take,skip);
+    }
+    
 }
