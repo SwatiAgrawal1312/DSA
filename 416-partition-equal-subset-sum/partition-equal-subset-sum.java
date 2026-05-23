ef62504391988146b6996f46fef1f01782ec9cee
Swati Agrawal <swati.agrawal1312@gmail.com>
2026-05-23 11:48:14 +0530

    Time: 119 ms (6.81%) | Memory: 72.7 MB (27.83%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -2,40 +2,34 @@ class Solution {
     Boolean dp[][];
     public boolean canPartition(int[] nums) {
         int s=0;
-
         for(int i=0;i<nums.length;i++){
             s+=nums[i];
-            }
+
+        }
         if(s%2!=0){
             return false;
         }
         int target=s/2;
-        dp=new Boolean[201][target+1];
-       
+        dp=new Boolean[nums.length][target+1];
+        return solve(nums,0,target);
         
-        return solve(nums,target,0);
     }
-    public boolean solve(int[] nums,int s,int i){
-        if(s==0){
+    public boolean solve(int nums[],int idx,int target){
+        if(target==0){
             return true;
         }
-        if(i>=nums.length){
+        if(idx>=nums.length){
             return false;
         }
-        if(dp[i][s]!=null){
-            return dp[i][s];
+        if(dp[idx][target]!=null){
+            return dp[idx][target];
         }
         boolean take=false;
-       if(s>=nums[i]){
-         take=solve(nums,s-nums[i],i+1);
-       }
-
-       
-       
-       boolean skip=solve(nums,s,i+1);
-       return dp[i][s]=take||skip;
-
-
-
+        if(target>=nums[idx]){
+            take=solve(nums,idx+1,target-nums[idx]);
+        }
+        boolean skip=solve(nums,idx+1,target);
+        return dp[idx][target]=take||skip;
     }
-}
+    
+} 
