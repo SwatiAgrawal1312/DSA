c4d4ff0068c05d60eecfbd7a45cce019d0ab0a29
Swati Agrawal <swati.agrawal1312@gmail.com>
2026-05-30 17:02:23 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (89.92%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -6,12 +6,15 @@ class Solution {
         int i=0;
         int j=1;
         while(j<nums.length){
+            
            if(nums[i]!=nums[j]){
             i++;
-                nums[i]=nums[j];
-                
+            nums[i]=nums[j];
+           
             }
             j++;
+            
+            
 
         }
         return i+1;
