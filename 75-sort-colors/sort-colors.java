6187112569680ee93ac2de9b055b1950008bc636
Swati Agrawal <swati.agrawal1312@gmail.com>
2026-05-31 11:41:45 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (9.64%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,35 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int mid=0;
+        int i=0;
+        int j=nums.length-1;
+        while(mid<=j){
+           
+             if(nums[mid]==2){
+                  int temp=nums[mid];
+                  nums[mid]=nums[j];
+                  nums[j]=temp;
+                
+                j--;
+
+
+
+            }else if(nums[mid]==0){
+                int temp=nums[mid];
+                  nums[mid]=nums[i];
+                  nums[i]=temp;
+                  i++;
+                  mid++;
+                  
+
+            }else {
+                mid++;
+
+            }
+          
+
+        }
+       
+        
+    }
+}
