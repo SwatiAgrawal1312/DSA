3bb580538716f581c593f79c1dfab784f52685c7
Swati Agrawal <swati.agrawal1312@gmail.com>
2026-06-06 08:37:07 +0530

    Time: 2 ms (99.92%) | Memory: 52.7 MB (27.62%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,22 +1,51 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        HashMap<Integer,Integer> hm=new HashMap<>();
-        List<Integer> res=new ArrayList<>();
+        int maj1=0;
+        int count1=0;
+        int maj2=0;
+        int count2=0;
         for(int i=0;i<nums.length;i++){
-            if(hm.containsKey(nums[i])){
-                hm.put(nums[i],hm.get(nums[i])+1);
+            if(nums[i]==maj1){
+                count1++;
+
+            }
+            else if(nums[i]==maj2){
+                count2++;
+
+            }
+            else if(count1==0){
+                maj1=nums[i];
+                count1=1;
+
+            }
+            else if(count2==0){
+                maj2=nums[i];
+                count2=1;
+
             }else{
-                hm.put(nums[i],1);
+                count1--;
+                count2--;
             }
         }
-        for(int x:hm.keySet()){
-        if(hm.get(x)>nums.length/3){
-            res.add(x);
+        ArrayList<Integer> l=new ArrayList<>();
+        // verification;
+        int freq1=0;
+        int freq2=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==maj1){
+                freq1++;
+            }else if(nums[i]==maj2){
+                freq2++;
+            }
 
         }
+        if(freq1>nums.length/3){
+            l.add(maj1);
         }
-        return res;
+        if(freq2>nums.length/3){
+            l.add(maj2);
+        }
+        return l;
         
     }
-    
 }
