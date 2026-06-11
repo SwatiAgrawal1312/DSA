7a520849728e020eb6299dd94339c7bb992a5049
Swati Agrawal <swati.agrawal1312@gmail.com>
2026-06-11 15:18:00 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (70.10%) - LeetSync

---
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -0,0 +1,56 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+/*
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+
+        if (head == null) return null;
+
+        HashMap<Node, Node> map = new HashMap<>();
+
+        Node temp = head;
+
+        // Create copy nodes
+        while (temp != null) {
+            map.put(temp, new Node(temp.val));
+            temp = temp.next;
+        }
+
+        temp = head;
+
+        // Connect next and random pointers
+        while (temp != null) {
+            map.get(temp).next = map.get(temp.next);
+            map.get(temp).random = map.get(temp.random);
+            temp = temp.next;
+        }
+
+        return map.get(head);
+    }
+}
