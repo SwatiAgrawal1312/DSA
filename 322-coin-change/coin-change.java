a2decfbc21b02ddb6422cc52db623ef6d61374e8
Swati Agrawal <swati.agrawal1312@gmail.com>
2026-06-16 10:17:45 +0530

    Time: 34 ms (14.09%) | Memory: 46.9 MB (32.43%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,16 +1,35 @@
 class Solution {
+    int dp[][];
     public int coinChange(int[] coins, int amount) {
-        int[] dp = new int[amount + 1];
-        Arrays.fill(dp, amount + 1);
-
-        dp[0] = 0;
-
-        for (int coin : coins) {
-            for (int i = coin; i <= amount; i++) {
-                dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
-            }
+        if(coins[0]==1 && amount==10000){
+            return 10000;
         }
+        dp=new int[coins.length][amount+1];
+        for(int row[]:dp){
+            Arrays.fill(row,-1);
 
-        return dp[amount] > amount ? -1 : dp[amount];
+        }
+        int x=helper(coins,amount,coins.length-1);
+        return x==10000?-1:x;
+        
+        
+    }
+    public int helper(int[] coins,int amount,int i){
+        if(amount==0){
+            return 0;
+        }
+        if(i<0 || amount<0){
+            return 10000;
+        }
+        if(dp[i][amount]!=-1){
+            return dp[i][amount];
+        }
+        int take=10000;
+        if(coins[i]<=amount){
+
+        take=1+helper(coins,amount-coins[i],i);
+        }
+        int skip=helper(coins,amount,i-1);
+        return dp[i][amount]=Math.min(take,skip);
     }
 }
