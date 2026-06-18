a5f1b02e7b97e0e99aad66c9d1108272cda148cf
Swati Agrawal <swati.agrawal1312@gmail.com>
2026-06-18 15:16:25 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (11.25%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,28 +1,32 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        HashSet<Integer> hs=new HashSet<>();
         List<List<Integer>> res=new ArrayList<>();
-        helper(nums,hs,new ArrayList<>(),res);
+        helper(nums,0,res);
         return res;
         
         
     }
-    public void helper(int[] nums,HashSet<Integer> hs,List<Integer> curr,List<List<Integer>> res){
-        if(curr.size()==nums.length){
-            res.add(new ArrayList<>(curr));
+    public void helper(int[] nums,int idx,List<List<Integer>> res){
+        if(idx==nums.length){
+            List<Integer> curr=new ArrayList<>();
+            for(int num:nums){
+                curr.add(num);
+            }
+            res.add(curr);
             return;
-        }
-        for(int i=0;i<nums.length;i++){
-           if(hs.contains(nums[i])){
-            continue;
-           }
-           hs.add(nums[i]);
-           curr.add(nums[i]);
-           helper(nums,hs,curr,res);
-           curr.remove(curr.size()-1);
-           hs.remove(nums[i]);
+
            
+        }
+        for(int i=idx;i<nums.length;i++){
+            swap(nums,idx,i);
+            helper(nums,idx+1,res);
+            swap(nums,idx,i);
 
         }
+    }
+    public void swap(int nums[],int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
     }
 }
