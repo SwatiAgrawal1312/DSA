51aa19df594b88169f6afd7fd90465add3b539bb
Swati Agrawal <swati.agrawal1312@gmail.com>
2026-06-23 14:28:07 +0530

    Time: 14 ms (70.23%) | Memory: 47.8 MB (31.81%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -4,13 +4,23 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
         }
-        List<Map.Entry<Integer,Integer>> list=new ArrayList<>(hm.entrySet());
-        Collections.sort(list,(a,b)->(b.getValue()-a.getValue()));
+        PriorityQueue<Map.Entry<Integer,Integer>> min_heap=new PriorityQueue<>((a,b)->a.getValue()-b.getValue());//by default min_heap
+
+        for(Map.Entry<Integer,Integer> x:hm.entrySet()){
+            min_heap.offer(x);
+            if(min_heap.size()>k){
+                min_heap.poll();
+            }
+
+        }
         int ans[]=new int[k];
-        for(int i=0;i<k;i++){
-            ans[i]=list.get(i).getKey();
+        for(int i=k-1;i>=0;i--){
+            ans[i]=min_heap.poll().getKey();
         }
         return ans;
+
         
     }
 }
+// TC:-o(n)+o(n)+o(nlogn)+o(k)
+// SC:-o(n)+o(n)+o(k)
