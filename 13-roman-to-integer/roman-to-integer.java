eced2479f12b5bd2d09f6e0a1d757de662aab70c
Swati Agrawal <swati.agrawal1312@gmail.com>
2026-06-29 08:09:20 +0530

    Time: 3 ms (78.09%) | Memory: 46 MB (98.30%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,36 +1,41 @@
 class Solution {
+     public int checkvalue(char ch){
+        switch(ch){
+        case 'I':return 1;
+        case 'V':return 5;
+        case 'X':return 10;
+        case 'L':return 50;
+        case 'C':return 100;
+        case 'D':return 500;
+        case 'M':return 1000;
+        default:return -1;
+    }
+    }
+   
     public int romanToInt(String s) {
-        int n=s.length();
-        int result=0;
-        for(int i=0;i<n;i++){
+        int ans=0;
+        int i=0;
+        while(i<s.length()){
             char ch=s.charAt(i);
-            if(i+1<n && getval(ch)<getval(s.charAt(i+1))){
-                result-=getval(ch);
-            }else{
-                result+=getval(ch);
+            if(i+1==s.length()){
+                ans+=checkvalue(ch);
+                break;
             }
+            if(checkvalue(ch)>=checkvalue(s.charAt(i+1))){
+
+                ans+=checkvalue(ch);
+                i+=1;
+
+            }
+            else {
+                ans+=checkvalue(s.charAt(i+1))-checkvalue(ch);
+                i+=2;
+            }
+
+
         }
-        return result;
+        return ans;
         
     }
-    public int getval(char ch){
-        switch(ch){
-            case 'I':return 1;
-             case 'V':return 5;
-            
-             case 'X':return 10;
-            
-             case 'L':return 50;
-            
-             case 'C':return 100;
-            
-             case 'D':return 500;
-              case 'M':return 1000;
-              default:return 0;
-            
-            
-
-        }
-
-    }
+   
 }
