f2c2843cd4835d32e9af83033f71f7011087137e
Swati Agrawal <swati.agrawal1312@gmail.com>
2026-06-29 19:40:44 +0530

    Time: 1 ms (62.63%) | Memory: 43.3 MB (50.64%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String checkcommon(String s1,String s2){
+        int n=Math.min(s1.length(),s2.length());
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<n;i++){
+            if(s1.charAt(i)==s2.charAt(i)){
+                sb.append(s1.charAt(i));
+
+            }else{
+                break;
+            }
+        }
+        return sb.toString();
+
+    }
+    public String longestCommonPrefix(String[] strs) {
+        String ans=strs[0];
+        for(int i=1;i<strs.length;i++){
+            ans=checkcommon(ans,strs[i]);
+        }
+        return ans;
+
+        
+    }
+}
