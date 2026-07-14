45d4a5906eabc6185ca340489afe3fd740c8cad3
Swati Agrawal <swati.agrawal1312@gmail.com>
2026-07-14 17:59:25 +0530

    Time: 139 ms (64.19%) | Memory: 111.3 MB (70.93%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.java
+++ 295-find-median-from-data-stream/find-median-from-data-stream.java
@@ -5,38 +5,38 @@ class MedianFinder {
 
     public MedianFinder() {
         max_heap=new PriorityQueue<>(Collections.reverseOrder());
-        min_heap=new PriorityQueue<>(); 
+        min_heap=new PriorityQueue<>();
+
         
     }
     
     public void addNum(int num) {
-        if(max_heap.isEmpty() || num<=max_heap.peek()){
-            max_heap.offer(num);
+        if(max_heap.isEmpty()|| num<max_heap.peek()){
+        max_heap.offer(num);
         }
         else{
-        min_heap.offer(num);
+            min_heap.offer(num);
         }
+        
         if(max_heap.size()>min_heap.size()+1){
             min_heap.offer(max_heap.poll());
         }
-        
-        if(min_heap.size()>max_heap.size()){
+        else if(max_heap.size()<min_heap.size()){
             max_heap.offer(min_heap.poll());
-        }
-        
 
-        
+        }
+       
         
     }
     
     public double findMedian() {
         if(max_heap.size()==min_heap.size()){
-        double ans=(double)max_heap.peek()+(double)min_heap.peek();
-        return ans/2;
+            return (float)(max_heap.peek()+min_heap.peek())/2;
         }
-        else{
-            return (double)max_heap.peek();
-        }
+        
+            return (float)max_heap.peek();
+        
+        
         
     }
 }
